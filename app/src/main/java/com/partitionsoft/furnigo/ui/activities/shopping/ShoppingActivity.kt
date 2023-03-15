fa2c3917fba16b9fae6cb39714c23e2a9da56f48
Yuriy Kravets <97830497+yuriykravets@users.noreply.github.com>
2023-03-15 21:52:47 +0200

    Initial commit

---
+++ app/src/main/java/com/partitionsoft/furnigo/ui/activities/shopping/ShoppingActivity.kt
@@ -0,0 +1,12 @@
+package com.partitionsoft.furnigo.ui.activities.shopping
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.partitionsoft.furnigo.R
+
+class ShoppingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping)
+    }
+}
